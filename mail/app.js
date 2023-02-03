bac3974864445c283eee0bec5891e9fef525d013
nicola00 <nicolajobs00@gmail.com>
2023-02-03 15:10:25 +0100

    FEAT add nuovaMail, leght

--- mail/app.js
+++ mail/app.js
@@ -4,5 +4,16 @@ console.log('array')
 const emailElement = ['marcocarta@gmail.com', 'paolorossi@gmail.com', 'nicolablog@gmail.com',
 'cartafoglio@gmail.com', 'arround@gmail.com', 'heartfree@gmail.com','maggiolino90@gmail.com']
 console.log(emailElement)
+
+console.log(emailElement.length)
+
+console.log(emailElement.length - 1)
+
+console.log(emailElement[3])
+
+let nuovaMail = 'michelepiccolo@gmail.com'
+emailElement.push(nuovaMail)
+console.log(emailElement)
+
 // Chiedi all’utente la sua email (con il prompt), controlla che sia nella lista di email,
 // stampa un messaggio appropriato sull’esito del controllo.
