b6c76a0db74e2bc0b561c93292a63935954f696f
nicola00 <nicolajobs00@gmail.com>
2023-02-03 17:05:31 +0100

    FEAT complete

--- gioco dei dadi/app.js
+++ gioco dei dadi/app.js
@@ -1,2 +1,33 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
+
+
+
+let btnUserElement = document.getElementById('btn-user')
+let numberUserElement = document.getElementById('number-user')
+let numberComputerElement = document.getElementById('number-computer')
+let risultElement = document.getElementById('risultato')
+
+
+
+btnUserElement.addEventListener('click', function () {
+    let numUser = Math.floor(Math.random() * 6) + 1
+    
+    
+    let numComputer = Math.floor(Math.random() * 6)
+    console.log(numUser, numComputer)
+
+    numberUserElement.innerHTML = numUser
+    
+    numberComputerElement.innerHTML = numComputer
+
+    if (numUser > numComputer){
+        console.log('hai vinto')
+        risultElement.innerHTML = 'Hai vinto!!!!!'
+    }
+    else {
+        console.log ('hai perso')
+        risultElement.innerHTML = 'Hai Perso!!!!!'
+    }
+})
+
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
