2b842e8e3a34ba86b69353aba40201f8e47661c5
nicola00 <nicolajobs00@gmail.com>
2023-02-03 15:04:47 +0100

    FEAT create mail

--- mail/app.js
+++ mail/app.js
@@ -1,5 +1,8 @@
 // MAIL
-
+console.log('array')
 // Crea un array con una lista di email (stringhe)
+const emailElement = ['marcocarta@gmail.com', 'paolorossi@gmail.com', 'nicolablog@gmail.com',
+'cartafoglio@gmail.com', 'arround@gmail.com', 'heartfree@gmail.com','maggiolino90@gmail.com']
+console.log(emailElement)
 // Chiedi all’utente la sua email (con il prompt), controlla che sia nella lista di email,
 // stampa un messaggio appropriato sull’esito del controllo.
