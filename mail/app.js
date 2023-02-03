7c69e197677619e06eda55dadec4818d0b95a293
nicola00 <nicolajobs00@gmail.com>
2023-02-03 16:31:29 +0100

    FEAT stampto il risultato

--- mail/app.js
+++ mail/app.js
@@ -1,20 +1,20 @@
 // MAIL
 console.log('array')
 // Crea un array con una lista di email (stringhe)
-const emailElement = ['marcocarta@gmail.com', 'paolorossi@gmail.com', 'nicolablog@gmail.com',
+let emailArray = ['marcocarta@gmail.com', 'paolorossi@gmail.com', 'nicolablog@gmail.com',
 'cartafoglio@gmail.com', 'arround@gmail.com', 'heartfree@gmail.com','maggiolino90@gmail.com']
-console.log(emailElement)
+console.log(emailArray)
 
-console.log(emailElement.length - 1)
+console.log(emailArray.length - 1)
 
-console.log(emailElement[3])
+console.log(emailArray[3])
 
 let nuovaMail = 'michelepiccolo@gmail.com'
-emailElement.push(nuovaMail)
-console.log(emailElement)
+emailArray.push(nuovaMail)
+console.log(emailArray)
 
-let numElement = emailElement.length
-console.log(emailElement.length)
+let numElement = emailArray.length
+console.log(emailArray.length)
 
 // Chiedi all’utente la sua email (con il prompt), controlla che sia nella lista di email,
 let mailUser = prompt('Inserisci la tua mail')
@@ -22,16 +22,25 @@ let mailUser = prompt('Inserisci la tua mail')
 
 // stampa un messaggio appropriato sull’esito del controllo.
 
-// for ( i = 0; i < numElement; i++) {
-// console.log(i, numElement, 'questo è il ciclo')
-
 let emailCorrect = document.getElementById('email-corretta')
 
-    if (emailElement === mailUser){
-        emailCorrect.innerHTML = mailUser + '' + 'La tua mail è stata verificata ed è corretta!!'
-        // console.log('risposta esatta')
-    } else {
-        emailCorrect.innerHTML = mailUser + '' + 'La mail inserita è errata :( Riprova...'
-        // console.log('risposta errata')
+for ( i = 0; i < numElement; i++) {
+    let singleMail = emailArray[i]
+    console.log(i, singleMail)
+    
+    if (singleMail === mailUser) {
+        console.log('risposta esatta')
+        emailCorrect.innerHTML = mailUser + ' ' + 'La tua mail è stata verificata ed è corretta!!'
     }
-// }
+    else {
+        emailCorrect.innerHTML = mailUser + ' ' + 'La mail inserita è errata :( Riprova...'
+    }
+}
+
+    
+        
+        
+        
+
+        
+    
